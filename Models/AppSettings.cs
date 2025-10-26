c4c9ba267f0e4a322719007e7b29d5fd3166cac3
farshadpart <f.sh.ss89@hotmail.com>
2025-10-26 14:57:42 +1030

    Protect the chat controller

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -18,7 +18,7 @@
     public record JWT {
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
-        public ushort ExpirationMinutes { get; set; }
+        public uint ExpirationMinutes { get; set; }
         public string SecretKey { get; set; } = string.Empty;
     }
 }
