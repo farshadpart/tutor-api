e2c99897fb933f67476c1c6fb714b0289cc2cc19
farshadpart <f.sh.ss89@hotmail.com>
2025-11-23 19:50:23 +1030

    Shortcut the email service

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -16,50 +16,52 @@ namespace Tutor.Api.Services
             _smtpClient = new SmtpClient(_smtp.Host, _smtp.Port);
         }
 
-        public async Task SendConfirmationLinkAsync(User user, string email, string confirmationLink)
+        public Task SendConfirmationLinkAsync(User user, string email, string confirmationLink)
         {
+            return Task.CompletedTask;
+            //var mailMessage = new MailMessage
+            //{
+            //    From = new MailAddress(_smtp.FromEmail),
+            //    Subject = "Email Confirmation",
+            //    Body = $"<p>Please confirm your email: {confirmationLink}</p>",
+            //    IsBodyHtml = true
+            //};
 
-            var mailMessage = new MailMessage
-            {
-                From = new MailAddress(_smtp.FromEmail),
-                Subject = "Email Confirmation",
-                Body = $"<p>Please confirm your email: {confirmationLink}</p>",
-                IsBodyHtml = true
-            };
+            //mailMessage.To.Add(email);
 
-            mailMessage.To.Add(email);
-
-            await _smtpClient.SendMailAsync(mailMessage);
+            //await _smtpClient.SendMailAsync(mailMessage);
         }
 
-        public async Task SendPasswordResetCodeAsync(User user, string email, string resetCode)
+        public Task SendPasswordResetCodeAsync(User user, string email, string resetCode)
         {
-            var mailMessage = new MailMessage
-            {
-                From = new MailAddress(_smtp.FromEmail),
-                Subject = "Password Reset Code",
-                Body = $"<p>Your password reset code is: <strong>{resetCode}</strong></p>",
-                IsBodyHtml = true
-            };
+            return Task.CompletedTask;
+            //var mailMessage = new MailMessage
+            //{
+            //    From = new MailAddress(_smtp.FromEmail),
+            //    Subject = "Password Reset Code",
+            //    Body = $"<p>Your password reset code is: <strong>{resetCode}</strong></p>",
+            //    IsBodyHtml = true
+            //};
 
-            mailMessage.To.Add(email);
+            //mailMessage.To.Add(email);
 
-            await _smtpClient.SendMailAsync(mailMessage);
+            //await _smtpClient.SendMailAsync(mailMessage);
         }
 
-        public async Task SendPasswordResetLinkAsync(User user, string email, string resetLink)
+        public Task SendPasswordResetLinkAsync(User user, string email, string resetLink)
         {
-            var mailMessage = new MailMessage
-            {
-                From = new MailAddress(_smtp.FromEmail),
-                Subject = "Password Reset Link",
-                Body = $"<p>You can reset your password by clicking this link: <a href='{resetLink}'>Reset Password</a></p>",
-                IsBodyHtml = true
-            };
+            return Task.CompletedTask;
+            //var mailMessage = new MailMessage
+            //{
+            //    From = new MailAddress(_smtp.FromEmail),
+            //    Subject = "Password Reset Link",
+            //    Body = $"<p>You can reset your password by clicking this link: <a href='{resetLink}'>Reset Password</a></p>",
+            //    IsBodyHtml = true
+            //};
 
-            mailMessage.To.Add(email);
+            //mailMessage.To.Add(email);
 
-            await _smtpClient.SendMailAsync(mailMessage);
+            //await _smtpClient.SendMailAsync(mailMessage);
         }
     }
 }
