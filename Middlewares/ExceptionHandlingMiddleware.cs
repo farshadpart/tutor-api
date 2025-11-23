0590000636643deb130bdb70ce6edbb8f04d71ec
farshadpart <55987163+farshadpart@users.noreply.github.com>
2025-11-23 19:21:30 +1030

    Add Serilog (#13)
    Co-authored-by: farshadpart <f.sh.ss89@hotmail.com>

---
+++ Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,57 @@
+﻿namespace Tutor.Api.Middlewares
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Tutor.Api.Models.Exceptions;
+
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (TutorException ex)
+            {
+                _logger.LogWarning(ex, "TutorException: {exceptionMessage}", ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception");
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var response = new
+                {
+                    Message = Errors.SOMETHING_WENT_WRONG
+                };
+
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
