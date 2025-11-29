9ac5b6ee4543e5d23fa18534cb71bcb5ab3d064e
farshadpart <55987163+farshadpart@users.noreply.github.com>
2025-11-30 10:11:43 +1030

    Add PrivacyAndPolicy method (#19)
    * Add PrivacyAndPolicy method
    
    * Remove unused namespaces
    
    ---------
    
    Co-authored-by: farshadpart <f.sh.ss89@hotmail.com>

---
+++ Controllers/LegalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutor.Api.Services.Legal;
+
+namespace Tutor.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LegalController : ControllerBase
+    {
+        [HttpGet("privacy")]
+        public async Task<IActionResult> Privacy()
+        {
+            return Ok(PrivacyPolicy.VALUE);
+        }
+    }
+}
