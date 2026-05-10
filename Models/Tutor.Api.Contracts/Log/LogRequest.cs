6a97b07ef6bad9ff14a974bbbfd9c97e75f95bc1
farshadpart <f.sh.ss89@gmail.com>
2026-05-10 16:17:14 +0930

    Add LogRequest to the source control

---
+++ Models/Tutor.Api.Contracts/Log/LogRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tutor.Api.Models.Tutor.Api.Contracts.Log
+{
+    public record LogRequest
+    (
+        [property: JsonConverter(typeof(JsonStringEnumConverter))]
+        LogLevel LogLevel, 
+        string? Exception,
+        string Message, 
+        object[] Arguments
+    );
+}
