debcfa5eb01f176542ec6e356cfb5600d00fca06
Farshad Shahsavari <f.sh.ss89@gmail.com>
2026-08-04 22:33:33 +0930

    Update ChatGptAudioClient

--- Tutor.Api/Services/ChatGptAudioClient.cs
+++ Tutor.Api/Services/ChatGptAudioClient.cs
@@ -1,3 +1,4 @@
+using System.ClientModel;
 using OpenAI.Audio;
 using Tutor.Api.Services.Interfaces;
 
@@ -5,11 +6,17 @@ namespace Tutor.Api.Services;
 
 public sealed class ChatGptAudioClient : IChatGptAudioClient
 {
-    private readonly AudioClient _client = new("whisper-1", Environment.GetEnvironmentVariable("TutorKey"));
+    private readonly AudioClient _whisperClient = new("whisper-1", Environment.GetEnvironmentVariable("TutorKey"));
+    private readonly AudioClient _speechClient = new("gpt-4o-mini-tts", Environment.GetEnvironmentVariable("TutorKey"));
 
     public string Transcribe(Stream audioStream, string fileName, AudioTranscriptionOptions options)
     {
-        var transcription = _client.TranscribeAudio(audioStream, fileName, options);
+        var transcription = _whisperClient.TranscribeAudio(audioStream, fileName, options);
         return transcription.Value.Text;
+    }
+
+    public async Task<ClientResult<BinaryData>> Speech(string text, CancellationToken token)
+    {
+        return await _speechClient.GenerateSpeechAsync(text, GeneratedSpeechVoice.Nova, null, token);
     }
 }
